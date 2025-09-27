07e46541377df2bd64b46f16363bd63787eea179
sagarsakharkar14 <sagarsakharkar14@yahoo.com>
2025-09-27 21:50:42 +0530

    tidays first commit

--- 1 Practice day 23-09-25/Proj1.py
+++ 1 Practice day 23-09-25/Proj1.py
@@ -5,50 +5,31 @@ import matplotlib.pyplot as plt
 
 URL = "https://raw.githubusercontent.com/marcopeix/MachineLearningModelDeploymentwithStreamlit/master/12_dashboard_capstone/data/quarterly_canada_population.csv"
 
-df = pd.read_csv('data/quarterly_canada_population.csv', dtype={'Quarter': str, 
-                            'Canada': np.int32,
-                            'Newfoundland and Labrador': np.int32,
-                            'Prince Edward Island': np.int32,
-                            'Nova Scotia': np.int32,
-                            'New Brunswick': np.int32,
-                            'Quebec': np.int32,
-                            'Ontario': np.int32,
-                            'Manitoba': np.int32,
-                            'Saskatchewan': np.int32,
-                            'Alberta': np.int32,
-                            'British Columbia': np.int32,
-                            'Yukon': np.int32,
-                            'Northwest Territories': np.int32,
-                            'Nunavut': np.int32})
+@st.cache_data
+def read_data():
+    df = pd.read_csv('data/quarterly_canada_population.csv', dtype={'Quarter': str, 
+                                'Canada': np.int32,
+                                'Newfoundland and Labrador': np.int32,
+                                'Prince Edward Island': np.int32,
+                                'Nova Scotia': np.int32,
+                                'New Brunswick': np.int32,
+                                'Quebec': np.int32,
+                                'Ontario': np.int32,
+                                'Manitoba': np.int32,
+                                'Saskatchewan': np.int32,
+                                'Alberta': np.int32,
+                                'British Columbia': np.int32,
+                                'Yukon': np.int32,
+                                'Northwest Territories': np.int32,
+                                'Nunavut': np.int32})
+    return df
 
-st.title("Population of Canada")
-st.markdown("Source table can be found [here](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901)")
 
-with st.expander("See full data table"):
-    st.write(df)
 
-with st.form("population-form"):
-    col1, col2, col3 = st.columns(3)
 
-    with col1:
-        st.write("Choose a starting date")
-        start_quarter = st.selectbox("Quarter", options=["Q1", "Q2", "Q3", "Q4"], index=2, key="start_q")
-        start_year = st.slider("Year", min_value=1991, max_value=2023, value=1991, step=1, key="start_y")
 
-    with col2:
-        st.write("Choose an end date")
-        end_quarter = st.selectbox("Quarter", options=["Q1", "Q2", "Q3", "Q4"], index=0, key="end_q")
-        end_year = st.slider("Year", min_value=1991, max_value=2023, value=2023, step=1, key="end_y")
-        
-    with col3:
-        st.write("Choose a location")
-        target = st.selectbox("Choose a location", options=df.columns[1:], index=0)
 
-    submit_btn = st.form_submit_button("Analyze", type="primary")
-
-start_date = f"{start_quarter} {start_year}"
-end_date = f"{end_quarter} {end_year}"
-
+@st.cache_data
 def format_date_for_comparison(date):
     if date[1] == 2:
         return float(date[2:]) + 0.25
@@ -59,6 +40,7 @@ def format_date_for_comparison(date):
     else:
         return float(date[2:])
 
+@st.cache_data
 def end_before_start(start_date, end_date):
     num_start_date = format_date_for_comparison(start_date)
     num_end_date = format_date_for_comparison(end_date)
@@ -67,6 +49,7 @@ def end_before_start(start_date, end_date):
         return True
     else:
         return False
+
 
 def display_dashboard(start_date, end_date, target):
     tab1, tab2 = st.tabs(["Population change", "Compare"])
@@ -111,10 +94,45 @@ def display_dashboard(start_date, end_date, target):
         fig.autofmt_xdate()
         st.pyplot(fig)
 
-if start_date not in df['Quarter'].tolist() or end_date not in df['Quarter'].tolist():
-    st.error("No data available. Check your quarter and year selection")
-elif end_before_start(start_date, end_date):
-    st.error("Dates don't work. Start date must come before end date.")
-else:
-    display_dashboard(start_date, end_date, target)
 
+
+
+if __name__=="__main__":
+    df = read_data()
+
+    st.title("Population of Canada")
+    st.markdown("Source table can be found [here](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901)")
+
+    with st.expander("See full data table"):
+        st.write(df)
+
+
+    with st.form("population-form"):
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.write("Choose a starting date")
+            start_quarter = st.selectbox("Quarter", options=["Q1", "Q2", "Q3", "Q4"], index=2, key="start_q")
+            start_year = st.slider("Year", min_value=1991, max_value=2023, value=1991, step=1, key="start_y")
+
+        with col2:
+            st.write("Choose an end date")
+            end_quarter = st.selectbox("Quarter", options=["Q1", "Q2", "Q3", "Q4"], index=0, key="end_q")
+            end_year = st.slider("Year", min_value=1991, max_value=2023, value=2023, step=1, key="end_y")
+            
+        with col3:
+            st.write("Choose a location")
+            target = st.selectbox("Choose a location", options=df.columns[1:], index=0)
+
+        submit_btn = st.form_submit_button("Analyze", type="primary")
+
+        
+    start_date = f"{start_quarter} {start_year}"
+    end_date = f"{end_quarter} {end_year}"
+
+    if start_date not in df['Quarter'].tolist() or end_date not in df['Quarter'].tolist():
+        st.error("No data available. Check your quarter and year selection")
+    elif end_before_start(start_date, end_date):
+            st.error("Dates don't work. Start date must come before end date.")
+    else:
+        display_dashboard(start_date, end_date, target)
--- 2 Caching/cache1.py
+++ 2 Caching/cache1.py
@@ -19,3 +19,18 @@ out = cache_this_function()
 streamlit.write(out)
 
 streamlit.subheader('st.chache_resource')
+
+@streamlit.cache_resource
+def create_simpole_limner_regression():
+    time.sleep(2)
+    X= np.array([1,2,3,4,5,6,7]).reshape(-1,1)
+    y = np.array([1,2,3,4,5,6,7])
+
+    model = LinearRegression().fit(X,y)
+    return model
+
+lr = create_simpole_limner_regression()
+X_pred = np.array([7]).reshape(-1,1)
+pred = lr.predict(X_pred)
+
+streamlit.write(f"{pred[0]}")
